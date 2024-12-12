ef4f3be4bf0047b0e89904bbe480201e84656788
Arxing <meiswind711108@gmail.com>
2024-12-12 20:12:41 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "particlefall",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "particlefall",
+            targets: ["particlefall"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "particlefall",
+            path: "./particlefall.xcframework"
+        ),
+    ]
+)
